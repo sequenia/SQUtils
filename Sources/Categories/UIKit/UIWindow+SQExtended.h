ea5772d7a78b9e4cf6e3270f665f5b3ce6c2d0cd
Nikolay Kagala <nick@sequenia.com>
2016-05-31 09:25:57 +0600

    UIWindow category was added with a method that animate changing of the rootvc

---
+++ Sources/Categories/UIKit/UIWindow+SQExtended.h
@@ -0,0 +1,15 @@
+//
+//  UIWindow+SQExtended.h
+//  SQUtils
+//
+//  Created by Nikolay Kagala on 31/05/16.
+//  Copyright © 2016 Nikolay Kagala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (SQExtended)
+
+- (void)setRootViewController: (UIViewController *)rootViewController animated: (BOOL)animated;
+
+@end
---
+++ Sources/Categories/UIKit/UIWindow+SQExtended.m
@@ -0,0 +1,37 @@
+//
+//  UIWindow+SQExtended.m
+//  SQUtils
+//
+//  Created by Nikolay Kagala on 31/05/16.
+//  Copyright © 2016 Nikolay Kagala. All rights reserved.
+//
+
+#import "UIWindow+SQExtended.h"
+
+@implementation UIWindow (SQExtended)
+
+- (void)setRootViewController: (UIViewController *)rootViewController animated: (BOOL)animated {
+    UIView *snapShotView;
+    
+    if (animated) {
+        snapShotView = [self snapshotViewAfterScreenUpdates: YES];
+        [rootViewController.view addSubview: snapShotView];
+    }
+    
+    [self setRootViewController: rootViewController];
+    
+    if (animated) {
+        [UIView animateWithDuration: 0.3 animations:^{
+            
+            snapShotView.layer.opacity = 0;
+            snapShotView.layer.transform = CATransform3DMakeScale(1.5, 1.5, 1.5);
+            
+        } completion:^(BOOL finished) {
+            
+            [snapShotView removeFromSuperview];
+            
+        }];
+    }
+}
+
+@end
